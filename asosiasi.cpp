17b9bd81991cef4bf6800eb5019502a8077fb68b
rukarajinbelajar <maar1101dnd@gmail.com>
2026-05-11 15:20:22 +0700

    membuat akses public dokter dan membuat parameter

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -4,5 +4,15 @@ using namespace std;
 
 class dokter;
 class pasien {
-    
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) :nama(pNama){
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
+    ~pasien(){
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
+    void tambahDokter(dokter*);
+    void cetakDokter();
 }
