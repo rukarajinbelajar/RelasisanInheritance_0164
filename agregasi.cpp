f140afa09179abd5ac5838f0d3f1b45cafcf1dc4
rukarajinbelajar <maar1101dnd@gmail.com>
2026-05-11 15:14:27 +0700

    program utama yang menghubungkan anak ke ibu, lalu menampilkan daftar anak masing masing ibu

--- agregasi.cpp
+++ agregasi.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 using namespace std;
 
-#include "AgregasiAnak.h"
-#include "AgregasiIbu.h"
+#include "Anak.h"
+#include "Ibu.h"
 
+int main(){
+    ibu* varIbu = new ibu("dini");
+    ibu* varIbu2 = new ibu("novi");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("rini");
+    anak* varAnak3 = new anak("Dewi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
+
+    varIbu->cetakAnak();
+    varIbu2->cetakAnak();
+
+    delete varIbu;
+    delete varIbu2;
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
+
+    system("pause");
+
+    return 0;
+}
